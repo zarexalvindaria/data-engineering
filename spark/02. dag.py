68feeea78324c9843ab61b1452611e9e7c2f4621
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-01-23 01:08:03 +0800

    docs: Add dag file
    Add dag exercise

---
+++ spark/02. dag.py
@@ -0,0 +1,4 @@
+# Create the DAG object
+dag = DAG(dag_id="car_factory_simulation",
+          default_args={"owner": "airflow","start_date": airflow.utils.dates.days_ago(2)},
+          schedule_interval="0 * * * *")
