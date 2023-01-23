acc77fc82b92f8a79763e0fb519f0c3b0bada947
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-01-24 03:21:04 +0800

    docs: Add transform exercise files
    Add transform exercise files.

---
+++ spark/06. transform-read-from-db.py
@@ -0,0 +1,33 @@
+# Get the rental rate column as a string
+rental_rate_str = film_df.____.____("____")
+
+# Split up and expand the column
+rental_rate_expanded = rental_rate_str.____.____("____", expand=True)
+
+# Assign the columns to film_df
+film_df = film_df.assign(
+    ____=____[____],
+    ____=____[____],
+)
+
+---------
+
+# Get the rental rate column as a string
+rental_rate_str = film_df.rental_rate.astype("str")
+
+# Split up and expand the column
+# rental_rate_expanded = rental_rate_str.____.____("____", expand=True)
+rental_rate_expanded = rental_rate_str.str.split(".", expand=True)
+
+# Assign the columns to film_df
+film_df = film_df.assign(
+    rental_rate_dollar=rental_rate_expanded[0],
+    rental_rate_cents=rental_rate_expanded[1],
+)
+
+
+exit
+quit/
+:q
+quit
+q!
