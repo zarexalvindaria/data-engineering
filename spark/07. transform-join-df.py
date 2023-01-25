8877c015cbd1db45bf929fed8339df2fd6f01dc4
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-01-25 22:15:18 +0800

    docs: Add transform join dataframe ex
    Add transform - join ratings df exercise and output.

---
+++ spark/07. transform-join-df.py
@@ -0,0 +1,25 @@
+# Use groupBy and mean to aggregate the column
+ratings_per_film_df = rating_df.____('____').____('____')
+
+# Join the tables using the film_id column
+film_df_with_ratings = film_df.join(
+    ratings_per_film_df,
+    film_df.film_id==____
+)
+
+# Show the 5 first results
+print(film_df_with_ratings.____)
+
+-----
+
+# Use groupBy and mean to aggregate the column
+ratings_per_film_df = rating_df.groupBy('film_id').mean('rating')
+
+# Join the tables using the film_id column
+film_df_with_ratings = film_df.join(
+    ratings_per_film_df,
+    film_df.film_id==ratings_per_film_df.film_id
+)
+
+# Show the 5 first results
+print(film_df_with_ratings.limit(5))
