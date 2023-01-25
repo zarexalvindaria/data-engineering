7e90c3c2d6328dbc1bb22e337b5cc3905b8a00bf
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-01-25 22:35:32 +0800

    docs: Add load to parquet exercise
    Add load to parquet exercise files.

---
+++ spark/08. load-to-parquet.py
@@ -0,0 +1,13 @@
+# Write the pandas DataFrame to parquet
+film_pdf.____("____")
+
+# Write the PySpark DataFrame to parquet
+film_sdf.____.____("____")
+
+------------
+
+# Write the pandas DataFrame to parquet
+film_pdf.to_parquet("films_pdf.parquet")
+
+# Write the PySpark DataFrame to parquet
+film_sdf.write.parquet(".films_pdf.parquet")
