4f0d6f26e0c7b003aa4364b86321dd762ebdb3d0
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-01-26 00:16:30 +0800

    docs: Add query table exercise
    Add query postgres table exercise.

---
+++ spark/12. query-table.py
@@ -0,0 +1,35 @@
+# Complete the connection URI
+connection_uri = "postgresql://____:____@____:____/datacamp_application"
+db_engine = sqlalchemy.create_engine(connection_uri)
+
+# Get user with id 4387
+user1 = pd.read_sql("SELECT * FROM rating ____ ____=____", db_engine)
+
+# Get user with id 18163
+user2 = pd.read_sql("SELECT * FROM rating ____ ____=____", db_engine)
+
+# Get user with id 8770
+user3 = pd.read_sql("SELECT * FROM rating ____ ____=____", db_engine)
+
+# Use the helper function to compare the 3 users
+print_user_comparison(____, ____, ____)
+
+
+----
+
+
+# Complete the connection URI
+connection_uri = "postgresql://repl:password@localhost:5432/datacamp_application"
+db_engine = sqlalchemy.create_engine(connection_uri)
+
+# Get user with id 4387
+user1 = pd.read_sql("SELECT * FROM rating WHERE user_id = 4387", db_engine)
+
+# # Get user with id 18163
+user2 = pd.read_sql("SELECT * FROM rating WHERE user_id = 18163", db_engine)
+
+# # Get user with id 8770
+user3 = pd.read_sql("SELECT * FROM rating WHERE user_id = 8770", db_engine)
+
+# # Use the helper function to compare the 3 users
+print_user_comparison(user1, user2, user3)
