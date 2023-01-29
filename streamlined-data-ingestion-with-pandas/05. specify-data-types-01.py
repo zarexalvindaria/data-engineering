fccd08375bce6ab8d6545c553170a9c948267c22
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-01-30 06:40:21 +0800

    docs: Add specify data types exercise 1
    Add specify data types exercise 1

---
+++ streamlined-data-ingestion-with-pandas/05. specify-data-types-01.py
@@ -0,0 +1,13 @@
+# Load csv with no additional arguments
+data = ____("vt_tax_data_2016.csv")
+
+# Print the data types
+print(____)
+
+# ---------------- #
+
+# Load csv with no additional arguments
+data = pd.read_csv("vt_tax_data_2016.csv")
+
+# Print the data types
+print(data.dtypes)
