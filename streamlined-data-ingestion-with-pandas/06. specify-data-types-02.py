e858a3277a307b3a77dff948ba5d8db9abc72f07
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-01-30 06:46:48 +0800

    docs: Add specify data types exercise 2
    Add specify data types exercise # 2.

---
+++ streamlined-data-ingestion-with-pandas/06. specify-data-types-02.py
@@ -0,0 +1,21 @@
+# Create dict specifying data types for agi_stub and zipcode
+data_types = {____,
+			  ____}
+
+# Load csv using dtype to set correct data types
+data = pd.read_csv("vt_tax_data_2016.csv", ____)
+
+# Print data types of resulting frame
+print(data.dtypes.head())
+
+# ------------- #
+
+# Create dict specifying data types for agi_stub and zipcode
+data_types = {"agi_stub": "category",
+			  "zipcode": str}
+
+# Load csv using dtype to set correct data types
+data = pd.read_csv("vt_tax_data_2016.csv", dtype=data_types)
+
+# Print data types of resulting frame
+print(data.dtypes.head())
