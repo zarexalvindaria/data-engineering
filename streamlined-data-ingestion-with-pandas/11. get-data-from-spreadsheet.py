cec0147a7eb0f82415f94e21c6c4cce2ed1c4fe6
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-02-01 09:56:46 +0800

    docs: Add loading data from spreadsheet exercise
    Add loading data from spreadsheet exercise.

---
+++ streamlined-data-ingestion-with-pandas/11. get-data-from-spreadsheet.py
@@ -0,0 +1,19 @@
+# Load pandas as pd
+____
+
+# Read spreadsheet and assign it to survey_responses
+survey_responses = ____
+
+# View the head of the dataframe
+print(____)
+
+# ------------ #
+
+# Load pandas as pd
+import pandas as pd
+
+# Read spreadsheet and assign it to survey_responses
+survey_responses = pd.read_excel("fcc_survey.xlsx")
+
+# View the head of the dataframe
+print(survey_responses.head())
