756f88f10f72712c540a6335b81d32329f56e10e
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-02-05 04:33:48 +0800

    docs: Add set boolean cols exercise 1
    Add set boolean columns exercise 1.

---
+++ streamlined-data-ingestion-with-pandas/19. set-boolean-columns.py
@@ -0,0 +1,13 @@
+# Load the data
+survey_data = pd.read_excel("fcc_survey_subset.xlsx")
+
+# Count NA values in each column
+print(survey_data.____)
+
+# ------ #
+
+# Load the data
+survey_data = pd.read_excel("fcc_survey_subset.xlsx")
+
+# Count NA values in each column
+print(survey_data.isna().sum())
