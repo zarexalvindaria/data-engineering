25251a40a3621b1a9b80959b490329d0386670d6
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-02-06 04:42:22 +0800

    docs: Add set boolean columns ex 2
    Add set boolean columns exercise 2.

---
+++ streamlined-data-ingestion-with-pandas/20. set-boolean-columns-2.py
@@ -0,0 +1,23 @@
+# Set dtype to load appropriate column(s) as Boolean data
+survey_data = pd.read_excel("fcc_survey_subset.xlsx",
+                            ____)
+
+# View financial burdens by Boolean group
+print(survey_data.groupby(____).sum())
+
+# ---------- #
+
+# Set dtype to load appropriate column(s) as Boolean data
+survey_data = pd.read_excel("fcc_survey_subset.xlsx",
+                            dtype={"HasDebt": bool})
+
+# View financial burdens by Boolean group
+print(survey_data.groupby("HasDebt").sum())
+
+
+
+
+
+
+
+
