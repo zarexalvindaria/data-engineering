7251a87dfd694981e7b2daa2f9eec3248694f622
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-02-10 18:37:22 +0800

    docs: Add working with agg functions ex 2
    Add working with aggregate functions exercise 2.

---
+++ streamlined-data-ingestion-with-pandas/34. working-with-aggregate-functions-02.py
@@ -0,0 +1,31 @@
+# Create a query to get month, max tmax, and min tmin by month
+query = """
+SELECT month, 
+	   MAX(tmax), 
+       ____
+  FROM weather 
+ GROUP BY month;
+"""
+
+# Get dataframe of monthly weather stats
+weather_by_month = pd.read_sql(query, engine)
+
+# View weather stats by month
+print(weather_by_month)
+
+# ------- #
+
+# Create a query to get month, max tmax, and min tmin by month
+query = """
+SELECT month, 
+	   MAX(tmax), 
+       MIN(tmax)
+  FROM weather 
+ GROUP BY month;
+"""
+
+# Get dataframe of monthly weather stats
+weather_by_month = pd.read_sql(query, engine)
+
+# View weather stats by month
+print(weather_by_month)
