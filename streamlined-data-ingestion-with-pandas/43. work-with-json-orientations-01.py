bbcd6d6f19c67c071c94a53e7e0bfec958b731f9
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-02-12 04:13:35 +0800

    docs: Add working with json orientations ex 01
    Add working with json orientations exercise 01.

---
+++ streamlined-data-ingestion-with-pandas/43. work-with-json-orientations-01.py
@@ -0,0 +1,27 @@
+try:
+    # Load the JSON without keyword arguments
+    df = ____
+    
+    # Plot total population in shelters over time
+    df["date_of_census"] = pd.to_datetime(df["date_of_census"])
+    df.plot(x="date_of_census", 
+            y="total_individuals_in_shelter")
+    plt.show()
+    
+except ValueError:
+    print("pandas could not parse the JSON.")
+
+# ---------- #
+
+try:
+    # Load the JSON without keyword arguments
+    df = pd.read_json('dhs_report_reformatted.json')
+    
+    # Plot total population in shelters over time
+    df["date_of_census"] = pd.to_datetime(df["date_of_census"])
+    df.plot(x="date_of_census", 
+            y="total_individuals_in_shelter")
+    plt.show()
+    
+except ValueError:
+    print("pandas could not parse the JSON.")
