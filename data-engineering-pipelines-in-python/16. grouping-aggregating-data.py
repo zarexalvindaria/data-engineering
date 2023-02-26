8e3ee372820d0c35622926fcf3dc16db03b908a2
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-02-27 03:33:07 +0800

    docs: Add grouping and aggregating ex
    Add grouping and aggregating exercise.

---
+++ data-engineering-pipelines-in-python/16. grouping-aggregating-data.py
@@ -0,0 +1,35 @@
+from pyspark.sql.functions import col, avg, stddev_samp, max as sfmax
+
+aggregated = (purchased
+              # Group rows by 'Country'
+              .____(col('Country'))
+              .agg(
+                # Calculate the average salary per group and rename
+                ____('Salary').____('average_salary'),
+                # Calculate the standard deviation per group
+                ____('Salary'),
+                # Retain the highest salary per group and rename
+                ____('Salary').____('____')
+              )
+             )
+
+aggregated.show()
+
+# ------------- #
+
+from pyspark.sql.functions import col, avg, stddev_samp, max as sfmax
+
+aggregated = (purchased
+              # Group rows by 'Country'
+              .groupBy(col('Country'))
+              .agg(
+                # Calculate the average salary per group and rename
+                avg('Salary').alias('average_salary'),
+                # Calculate the standard deviation per group
+                stddev_samp('Salary'),
+                # Retain the highest salary per group and rename
+                sfmax('Salary').alias('highest_salary')
+              )
+             )
+
+aggregated.show()
