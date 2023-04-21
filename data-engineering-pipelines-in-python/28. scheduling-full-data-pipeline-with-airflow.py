b8e650d1e122200279d839f457399feacc77fb19
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-04-22 00:09:41 +0800

    docs: Add scheduling full data pipeline with Airflow ex
    Add scheduling full data pipeline in Airflow exercise.

---
+++ data-engineering-pipelines-in-python/28. scheduling-full-data-pipeline-with-airflow.py
@@ -0,0 +1,23 @@
+spark_args = {"py_files": dependency_path,
+              "conn_id": "spark_default"}
+# Define ingest, clean and transform job.
+with dag:
+    ingest = ____(task_id='Ingest_data', bash_command='tap-marketing-api | target-csv --config %s' % config)
+    clean = ____(application=clean_path, task_id='clean_data', **spark_args)
+    insight = ____(application=transform_path, task_id='show_report', **spark_args)
+    
+    # set triggering sequence
+    ____ >> ____ >> ____
+
+# ------------ #
+
+spark_args = {"py_files": dependency_path,
+              "conn_id": "spark_default"}
+# Define ingest, clean and transform job.
+with dag:
+    ingest = BashOperator(task_id='Ingest_data', bash_command='tap-marketing-api | target-csv --config %s' % config)
+    clean = SparkSubmitOperator(application=clean_path, task_id='clean_data', **spark_args)
+    insight = SparkSubmitOperator(application=transform_path, task_id='show_report', **spark_args)
+    
+    # set triggering sequence
+    ingest >> clean >> insight
