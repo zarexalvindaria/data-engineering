b058c8330909e07f5f9c5a4f42ea7df6cfaf7719
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-06-04 22:27:02 +0800

    docs: Add comment per step
    Add comment per step to clarify what it does in the program.

--- exercises/backend-challenge/main.py
+++ exercises/backend-challenge/main.py
@@ -1,24 +1,33 @@
-# Load pandas as pd
+# Load required libraries
 import pandas as pd
 import requests
 import json
 import re
 
+"""This function counts the number of ages in the dataframe
+ that are greater than or equal to 50"""
 def count_age(url):
 
-    r = requests.get(url)
-    data = r.text
+    # Get the data from the api
+    data = requests.get(url).text
+
+    # Parse the data and convert it to object
     json_data = json.loads(data)
 
-    # Find all matches of the pattern `key=(\w+), age=(\w+),?` in the `json_data['data']` string.
+    # Find all the matches of the pattern `key=(\w+), age=(\w+),?` in the `json_data['data']` string
     matches = re.findall("key=(\w+), age=(\w+),?", json_data['data'])
 
-    # Create a DataFrame with two columns, one for the `key` and one for the `age`.
+    # Create a DataFrame with `key` and `age` columns
     df = pd.DataFrame(matches, columns=['key', 'age'])
 
+    # Convert the data type of age to integer to allow querying
     df = df.astype({'age':'int'})
 
-    result = df.query('age >= 50')
+    # Filter the data frame with ages 50 or higher
+    filtered_df = df.query('age >= 50')
+
+    # Store the count of rows with age of 50 or higher in the result variable
+    result = filtered_df.agg(['count'], axis='index')
     
     # return the rows with ages greater than or equal to 50
     return result
